319022d8dfcb3806411897197605b60a908971f4
Andrejs Popovs <andrejs.popovs@prima.dev>
2020-09-07 13:42:24 +0300

    strongPasswordChecker test cases added

--- scala-playground/src/test/scala/leetcode/StrongPasswordCheckerSpec.scala
+++ scala-playground/src/test/scala/leetcode/StrongPasswordCheckerSpec.scala
@@ -2,10 +2,13 @@ package leetcode
 
 import leetcode.StrongPasswordChecker.strongPasswordChecker
 import org.scalatest._
+
 class StrongPasswordCheckerSpec extends FlatSpec with Matchers {
-  "StrongPasswordChecker" must
-    "check pass" in {
+  "StrongPasswordChecker" must "check pass" in {
     strongPasswordChecker("1111111111") shouldBe 3
     strongPasswordChecker("1234567890123456Baaaaa") shouldBe 3
+    strongPasswordChecker("ABABABABABABABABABAB1") shouldBe 2
+    strongPasswordChecker("aaaaaaaaaaaaaaaaaaaaa") shouldBe 7
+    strongPasswordChecker("aaa123") shouldBe 1
   }
 }
