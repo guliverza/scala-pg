5cad011f2b4ee247a9de8c5517bc1c35f31177af
Andrejs Popovs <andrejs.popovs@prima.dev>
2020-09-07 13:37:09 +0300

    strongPasswordChecker

--- scala-playground/src/main/scala/leetcode/StrongPasswordChecker.scala
+++ scala-playground/src/main/scala/leetcode/StrongPasswordChecker.scala
@@ -1,6 +1,19 @@
 package leetcode
 
 object StrongPasswordChecker {
+  /**A password is considered strong if below conditions are all met:
+
+    It has at least 6 characters and at most 20 characters.
+    It must contain at least one lowercase letter, at least one uppercase letter, and at least one digit.
+    It must NOT contain three repeating characters in a row
+      ("...aaa..." is weak, but "...aa...a..." is strong, assuming other conditions are met).
+    Write a function strongPasswordChecker(s), that takes a string s as input,
+      and return the MINIMUM change required to make s a strong password.
+      If s is already strong, return 0.
+
+    Insertion, deletion or replace of any one character are all considered as one change.
+   *
+   */
   def strongPasswordChecker(s: String): Int = {
     val required = Seq(
       s.exists(c => c.isLetter && c.isLower),
@@ -8,12 +21,9 @@ object StrongPasswordChecker {
       s.exists(c => c.isDigit)).count(b => !b)
     val anyToAdd = if (s.length < 6) 6 - s.length else 0
     val extra = if (s.length > 20) s.length - 20 else 0
-    val toSplit = if (s.length >= 3) split(s).map(word => word.length-2).sum else 0
+    val toSplit = if (s.length >= 3) split(s).map(word => word.length/3).sum else 0
     println(required, anyToAdd, toSplit, extra)
-    if (required == 0 || extra == 0)
-      Seq(required, anyToAdd, toSplit, extra).max
-    else
-      Seq(required+extra, anyToAdd, toSplit, extra).max
+    Seq(required, anyToAdd, toSplit, extra).max
   }
 
   def split(s: String): List[String] = {
--- scala-playground/src/test/scala/leetcode/StrongPasswordCheckerSpec.scala
+++ scala-playground/src/test/scala/leetcode/StrongPasswordCheckerSpec.scala
@@ -6,5 +6,6 @@ class StrongPasswordCheckerSpec extends FlatSpec with Matchers {
   "StrongPasswordChecker" must
     "check pass" in {
     strongPasswordChecker("1111111111") shouldBe 3
+    strongPasswordChecker("1234567890123456Baaaaa") shouldBe 3
   }
 }
