9d6b8c23304fe0d1fda986c439229da5128126b1
Andrejs Popovs <andrejs.popovs@prima.dev>
2020-09-08 18:25:36 +0300

    closest palindrom

---
+++ scala-playground/src/main/scala/leetcode/ClosestPalindrome.scala
@@ -0,0 +1,47 @@
+package leetcode
+
+object ClosestPalindrome {
+  /**
+   * Given an integer n, find the closest integer (not including itself), which is a palindrome.
+   *
+   * The 'closest' is defined as absolute difference minimized between two integers.
+   *
+   * Example 1:
+   *
+   * Input: "123"
+   * Output: "121"
+   */
+
+  def nearestPalindromic(n: String): String = {
+    val input = n.toLong
+    val leftWithMiddle = n.take(math.round(n.length / 2.0f)).toLong
+    val haveMiddle = n.length % 2 == 1
+    val palindroms1 = for {
+      left <- leftWithMiddle - 1 to leftWithMiddle + 1
+    } yield {
+      val right = (if (haveMiddle) left / 10 else left).toString.reverse
+      (left + right).toLong
+    }
+    val palindroms2 = (-2 to 2).collect {
+      case x if input + x >= 0 => input + x
+    } map (x => mirror(x.toString).toLong)
+    val best = (palindroms1 ++ palindroms2).fold(input) {
+      case (res, `input`) => res
+      case (`input`, res1) if res1 != input => res1
+      case (res, res1) =>
+        val diff = math.abs(res - input)
+        val diff1 = math.abs(res1 - input)
+        if (diff1 < diff || (diff1 == diff && res1 < input)) res1
+        else res
+    }
+    best.toString
+  }
+
+  def mirror(n: String): String = {
+    val left = n.take(n.length / 2)
+    val newRight = n.take(n.length / 2).reverse
+    val middle = if (n.length % 2 == 0) ""
+    else n(n.length / 2)
+    left + middle + newRight
+  }
+}
---
+++ scala-playground/src/test/scala/leetcode/ClosestPalindromeSpec.scala
@@ -0,0 +1,22 @@
+package leetcode
+
+import leetcode.ClosestPalindrome.nearestPalindromic
+import org.scalatest._
+
+class ClosestPalindromeSpec extends FlatSpec with Matchers {
+  "ClosestPalindrome" must "find lines" in {
+    nearestPalindromic("123") should be("121")
+    nearestPalindromic("1") should be("0")
+    nearestPalindromic("230") should be("232")
+    nearestPalindromic("987099999") should be("987101789")
+    nearestPalindromic("11") should be("9")
+    nearestPalindromic("11011") should be("11111")
+    nearestPalindromic("358764513820540928") should be("358764513315467853")
+    nearestPalindromic("1837722381") should be("1837667381")
+    nearestPalindromic("99") should be("101")
+    nearestPalindromic("999") should be("1001")
+    nearestPalindromic("998") should be("999")
+    nearestPalindromic("997") should be("999")
+    nearestPalindromic("996") should be("999")
+  }
+}
