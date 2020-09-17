92bae3f23335ba35c899ff608ce945dd2aacbab0
Andrejs Popovs <apopov@evolutiongaming.com>
2020-09-17 13:20:26 +0300

    some playing with max_xor

--- src/main/scala/leetcode/september2020/common/BinTree.scala
+++ src/main/scala/leetcode/september2020/common/BinTree.scala
@@ -1,6 +1,25 @@
 package leetcode.september2020.common
 
-case class BinTree(value: Int, var left: Option[BinTree] = None, var right: Option[BinTree] = None)
+case class BinTree(value: Int, var left: Option[BinTree] = None, var right: Option[BinTree] = None) {
+  def print(buffer: StringBuilder, prefix: String, childrenPrefix: String): Unit = {
+    buffer.append(prefix)
+    buffer.append(value)
+    buffer.append('\n')
+    val it = (left ++ right).iterator
+    while ( {
+      it.hasNext
+    }) {
+      val next = it.next
+      if (it.hasNext) next.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ")
+      else next.print(buffer, childrenPrefix + "└── ", childrenPrefix + "    ")
+    }
+  }
+  def printTree() = {
+    val result = new StringBuilder()
+    print(result, "", "")
+    println(result)
+  }
+}
 
 object BinTree {
 }
--- src/main/scala/leetcode/september2020/week3/MaximumXOR.scala
+++ src/main/scala/leetcode/september2020/week3/MaximumXOR.scala
@@ -1,28 +1,55 @@
 package leetcode.september2020.week3
 
+import leetcode.september2020.common.BinTree
+
 object MaximumXOR {
 
   def findMaximumXOR(nums: Array[Int]): Int = {
-    val high = highestBit(nums.max)
-    val (less, more) = nums.toList.partition(_ < high)
-    val (list1, list2) = if (less.isEmpty || more.isEmpty) {
-      (nums.toList, nums.toList)
-    } else {
-      (less, more)
-    }
-    list1.foldLeft(0) { case (max, n1) =>
-      math.max(max, list2.foldLeft(0) {
-        case (max2, n2) => math.max(max2, n1 ^ n2)
-      })
-    }
+    println("INPUT: " + nums.toList)
+    val bit = highestBit(nums.max)
+    println(s"max = ${nums.max}")
+    println(s"bit = $bit")
+    val root = BinTree(0)
+    nums.foreach(num => putBinaryToTree(num, root, bit))
+    root.printTree()
+    findMax(root.left, root.right, bit)
+  }
+
+  def findMax(left: Option[BinTree], right: Option[BinTree], hiBit: Int): Int = (left, right) match {
+    case (Some(left), Some(right)) =>
+      val bit = 1 << hiBit
+      ((left.value & bit) ^ (right.value & bit))+ Seq(
+        findMax(left.left, right.left, hiBit - 1),
+        findMax(left.left, right.right, hiBit - 1),
+        findMax(left.right, right.left, hiBit - 1),
+        findMax(left.right, right.right, hiBit - 1),
+      ).max
+    case (Some(left), _) =>
+      findMax(left.left, left.right, hiBit - 1)
+    case (_, Some(right)) =>
+      findMax(right.left, right.right, hiBit - 1)
+    case _ =>
+      0
   }
 
   def highestBit(number: Int): Int = {
-    val bit = (0 to 31).find(bit => (1 << bit) >= number)
+    val bit = (0 to 31).find(bit => (1 << bit) > number)
     bit.map { bit =>
-      1 << (bit - 1)
+      bit-1
     }.getOrElse {
-      1 << 31
+      30
+    }
+  }
+
+  def putBinaryToTree(num: Int, node: BinTree, hiBit: Int): Unit = {
+    if (hiBit >= 0) {
+      if ((num & (1 << hiBit)) == 0) {
+        node.left = Some(node.left.getOrElse(BinTree(num)))
+        putBinaryToTree(num, node.left.get, hiBit - 1)
+      } else {
+        node.right = Some(node.right.getOrElse(BinTree(num)))
+        putBinaryToTree(num, node.right.get, hiBit - 1)
+      }
     }
   }
 }
