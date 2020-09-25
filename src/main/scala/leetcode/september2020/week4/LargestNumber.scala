0170679194815a9774b2444afed2d9e95989007f
Andrejs Popovs <apopov@evolutiongaming.com>
2020-09-25 22:05:13 +0300

    largest number simplified

--- src/main/scala/leetcode/september2020/week4/LargestNumber.scala
+++ src/main/scala/leetcode/september2020/week4/LargestNumber.scala
@@ -1,25 +1,10 @@
 package leetcode.september2020.week4
 
-import scala.annotation.tailrec
-
 object LargestNumber {
   def largestNumber(nums: Array[Int]): String = {
-    val res = largest(nums.toList.map(_.toString), 9, "")
+    val res = nums.map(_.toString).sorted(largestOrdering).mkString("")
     if (res.forall(_ == '0')) "0"
     else res
-  }
-
-  @tailrec
-  def largest(nums: List[String], digit: Int, result: String): String = {
-    if (nums.isEmpty) {
-      result
-    } else if (digit == 0) {
-      result + nums.mkString("")
-    } else {
-      val (candidates, less) = nums.partition(_.startsWith(digit.toString))
-      val sorted = candidates.sorted(largestOrdering)
-      largest(less, digit - 1, result + sorted.mkString(""))
-    }
   }
 
   val largestOrdering: Ordering[String] = (a: String, b: String) => {
@@ -29,5 +14,4 @@ object LargestNumber {
       (b + a).compareTo(a + b)
     }
   }
-
 }
