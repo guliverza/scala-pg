22c530fe31947508bf7090915ea0fad0e3a3da69
Andrejs Popovs <andrejs.popovs@prima.dev>
2020-10-01 16:03:06 +0300

    RecentCounter: optimization

--- src/main/scala/leetcode/october2020/week1/RecentCounter.scala
+++ src/main/scala/leetcode/october2020/week1/RecentCounter.scala
@@ -3,7 +3,7 @@ package leetcode.october2020.week1
 class RecentCounter {
   var pings: List[Int] = Nil
   def ping(t: Int): Int = {
-    pings = t :: pings
-    pings.takeWhile(_ >= t - 3000).size
+    pings = t :: pings.takeWhile(_ >= t - 3000)
+    pings.size
   }
 }
